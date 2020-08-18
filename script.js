e1a26191a53c3d071153989f3607a4202b469780
Soua Xiong <68033651+sxiong6901@users.noreply.github.com>
2020-08-18 07:57:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function updatetime() {
   for(var i = 0; i < timeOfday.length; i++) {
 
     if (parseInt(timeOfday[i]) > currentTime) {
-      $("#" + timeOfday[i]).attr("style", "background-color: salmon");
+      $("#" + timeOfday[i]).attr("style", "background-color: lightgreen");
       
     }
     else if (parseInt(timeOfday[i]) < currentTime) {
